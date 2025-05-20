b0e33b03951850cb58048079ed673b50e3d76f98
Littl3Fox <102700820+Littl3Fox@users.noreply.github.com>
2025-05-19 21:29:43 -0300

    Adicionado um dataset para treinamento e tratamento das frases e sentimentos

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import nltk
+import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
 def main():
     #Aqui vai a frase/ entrada do usuário/ base de dados. Por agora será uma frase pré definida para testes
-    frase = "O dia está bonito hoje"
-
-    #Eu tenho que limpar o texto para treinar o modelo
-    #Nisso irei tirar pontuação, stop-words(palavras que não tem tanto significado) e uniformizar o texto(deixar tudo minúsculo)
-    #Nisso tenho que tokenizar o texto(é praticamente transformar cada palavra/pontuação em um elemento de uma lista)
-    palavras = word_tokenize(frase.lower(), language="portuguese")
+    dataset = open("dataset.txt","r",  encoding="utf-8")
 
     #Carrego as stop-words da biblioteca
     stop_words = set(stopwords.words('portuguese'))
 
-    #Removo as stop-words
-    palavras_filtradas = [palavra for palavra in palavras if palavra not in stop_words]
+    #Eu tenho que limpar o texto para treinar o modelo
+    #Nisso irei tirar pontuação, stop-words(palavras que não tem tanto significado) e uniformizar o texto(deixar tudo minúsculo)
+    #Nisso tenho que tokenizar o texto(é praticamente transformar cada palavra/pontuação em um elemento de uma lista)
+    frases = []
 
-    print(palavras_filtradas)
+    for linha in dataset:
+
+        #Divido o texto do rótulo
+        texto, sentimento = linha.split(";")
+        sentimento = sentimento.strip()
+
+        #Transformo as palavras em minusculas e token
+        tokens = word_tokenize(texto.lower(), language="portuguese")
+
+        #Tiro stopwords da frase e pontuação
+        tokens_filtrados = [palavra for palavra in tokens if palavra not in stop_words and palavra not in string.punctuation]
+
+        #Faço uma lista de dicionario com a frase e seu respectivo sentimento
+        frases.append({
+            "frase": tokens_filtrados,
+            "sentimento": sentimento
+        })
+
+    
+
+    print(frases)
+
+    
     return
 
 
